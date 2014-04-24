8aeb2eccc7252db4ee0629fed8521a42d907348c
mbibos <mbibos@excilys.com>
2014-04-24 11:16:15 +0200

    criteria sur company

--- computer-database-persistance/src/main/java/com/excilys/dao/impl/CompanyDAOImpl.java
+++ computer-database-persistance/src/main/java/com/excilys/dao/impl/CompanyDAOImpl.java
@@ -5,6 +5,9 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,16 +38,16 @@ public class CompanyDAOImpl implements ICompanyDAO{
 	
 	public Company findById(long id) {
 		logger.info("attempting to find a company by id");
-		String hql = "from Company c where c.id = :compId";
-		try {
-			Query query =  em.createQuery(hql);
-			query.setParameter("compId", id);
-		    return (Company) query.getResultList().get(0);
-		} catch (DataAccessException e) {
-			logger.debug("failed to found a company by id "+e.getMessage());
-			throw new IllegalPersonnalException();
-		}
+		
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Company> criteria = builder.createQuery(Company.class );
+		Root<Company> company = criteria.from(Company.class );
+
+		criteria.select(company);
+		criteria.where( builder.equal( company.get("id"), id ) );
+		return em.createQuery(criteria).getResultList().get(0);
 	}
+	
 	@SuppressWarnings("unchecked")
 	public List<Company> findAll() {
 		logger.info("attempting to find a company by id");
