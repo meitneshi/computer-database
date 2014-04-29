c229e14767a874ec9503a644ce060b3126656639
mbibos <mbibos@excilys.com>
2014-04-29 17:51:17 +0200

    passage a jax Rs qui est en fait jax ws, grosse confusion, la release 3.7.0 corrspondait a jax rs et maintenant on fait jax ws

--- computer-database-webservices/src/main/java/com/excilys/webService/ComputerWebService.java
+++ computer-database-webservices/src/main/java/com/excilys/webService/ComputerWebService.java
@@ -2,21 +2,27 @@ package com.excilys.webService;
 
 import java.util.List;
 
-import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.excilys.om.Computer;
 import com.excilys.service.IComputerService;
 
 @WebService
+@Component
+@Path("/webServices")
 public class ComputerWebService {
 
 	@Autowired
 	private IComputerService services;
 
-	@WebMethod
+	@GET
+	@Produces("application/xml")
 	public List<Computer> findAll() {
 		return services.findAll();
 	}
