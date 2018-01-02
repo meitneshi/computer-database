657d6ed847b1f28f1378ecd7959bb4980f8c1075
mbibos <mbibos@e-biz.fr>
2018-01-02 14:47:50 +0100

    add failed test

---
+++ computer-database-binding/src/test/java/com/excilys/mapper/Test.java
@@ -0,0 +1,12 @@
+package java.com.excilys.mapper;
+
+import static org.junit.Assert.assertEquals;
+
+public class Test {
+
+    @org.junit.Test
+    public void toDto() {
+        assertEquals("toto", true, 10);
+    }
+
+}
